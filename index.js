9c380d1354365ed3630310cdfe1d416f41d0042c
kwme <aob.attaporn@gmail.com>
2023-06-04 15:18:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -25,24 +25,60 @@ async function test() {
     //     links.push({ "link": link, name: name });
     // });
     linkss.each((index, value) => {
-        links.push({ link: $(value).attr("href").replace('./yr', 'https://mapdb.cncnet.org/yr'), name: $(value).text() });
+        links.push({ link: $(value).attr("href").replace('./yr', 'https://mapdb.cncnet.org/yr'), name: $(value).attr("href").split('/')[2] });
 
     })
 
-    for (const rr of links) {
-        const file = fs.createWriteStream(`file/${rr.name.replace(/\s/g, '-')}.zip`);
-        const request = http.get(rr.link, async function(response) {
-            response.pipe(file);
+    let i = 1;
 
-            // after download completed close filestream
-            file.on("finish", async() => {
-                file.close();
-                console.log("Download Completed");
+    const perChunk = 100 // items per chunk    
+
+    const inputArray = links
+
+    const result = inputArray.reduce((resultArray, item, index) => {
+        const chunkIndex = Math.floor(index / perChunk)
+
+        if (!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+        }
+
+        resultArray[chunkIndex].push(item)
+
+        return resultArray
+    }, [])
+
+    // console.log(result)
+    for (const rrr of result) {
+        let promises = [];
+
+        rrr.forEach(function(rr) {
+            let rettt = `${i}.Download Completed`;
+            const proo = new Promise((resolve, reject) => {
+                const file = fs.createWriteStream(`file/${rr.name}`);
+                const request = http.get(rr.link, async function(response) {
+                    response.pipe(file);
+                    file.on("finish", function() {
+                        file.close(() => {
+                            // console.log(`${i}.Download Completed`);
+                            resolve(rettt);
+                        });
+                    });
+                    file.on("error", function(err) {
+                        fs.unlink(pipFilePath);
+                        reject(err);
+                    });
+                });
             });
+            i++;
+            promises.push(proo)
+        });
+        await Promise.all(promises).then(function(results) {
+            console.log(results)
+            results.forEach(function(response) {
+                console.log(response)
+            })
 
         });
-        await delay();
-        return
     }
 
 }
